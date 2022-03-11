9abd482afe1a416065d11de415401216dce25c6b
Park Joyeong <drhamst@gmail.com>
2022-03-11 17:32:23 +0900

    Project init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: ["plugin:@typescript-eslint/recommended", "plugin:import/errors", "plugin:import/warnings", "plugin:import/typescript", "plugin:prettier/recommended", "prettier", "plugin:storybook/recommended"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    project: ["./tsconfig.json"]
+  },
+  rules: {
+    "prettier/prettier": ["error", {
+      endOfLine: "auto"
+    }]
+  }
+};
