84c19a6bbaabbf979d90c2632a88353ffa06fb70
Park Joyeong <drhamst@gmail.com>
2022-04-07 21:00:49 +0900

    Making CalendarBody

--- src/components/Calendar/CalendarComponents/CalendarBody.tsx
+++ src/components/Calendar/CalendarComponents/CalendarBody.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import monthsData from "./months.json";
 
 export interface Props {
     selectedYYYYMM: string;
@@ -6,6 +7,11 @@ export interface Props {
 
 const CalendarBody = (props: Props) => {
     const firstDay = 1;
+    const weekdayIdxOfFirstDay = new Date(
+        parseInt(props.selectedYYYYMM.substring(0, 4)),
+        parseInt(props.selectedYYYYMM.substring(4, 6)),
+        0,
+    ).getDay();
     const lastDay = new Date(
         parseInt(props.selectedYYYYMM.substring(0, 4)),
         parseInt(props.selectedYYYYMM.substring(4, 6)),
@@ -13,15 +19,23 @@ const CalendarBody = (props: Props) => {
     ).getDate();
     const tableData: JSX.Element[] = [];
     const setTableData = () => {
-        tableData.push(<tr>);
-        for(let i=firstDay; i<=lastDay; i++) {
-         console.log();   
+        const rowData = [];
+        let weekdayIdx = weekdayIdxOfFirstDay;
+        for (let i = firstDay; i <= lastDay; i++) {
+            rowData.push(<td>{i}</td>);
+            weekdayIdx++;
+            if (weekdayIdx === 7) {
+                tableData.push(<tr>{rowData}</tr>);
+            }
         }
-        tableData.push(</tr>);
     };
     setTableData();
     return (
         <div>
+            <h1>
+                {(monthsData as any)[props.selectedYYYYMM.substring(4, 6)]}&nbsp;
+                {props.selectedYYYYMM.substring(0, 4)}
+            </h1>
             <table>{tableData}</table>
         </div>
     );
