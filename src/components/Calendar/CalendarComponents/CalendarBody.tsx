110325b75373ecbe08eccb221b13c1ba65cbc48b
Park Joyeong <drhamst@gmail.com>
2022-04-11 18:10:42 +0900

    Complete initial calendar body

--- src/components/Calendar/CalendarComponents/CalendarBody.tsx
+++ src/components/Calendar/CalendarComponents/CalendarBody.tsx
@@ -9,9 +9,9 @@ const CalendarBody = (props: Props) => {
     const firstDay = 1;
     const weekdayIdxOfFirstDay = new Date(
         parseInt(props.selectedYYYYMM.substring(0, 4)),
-        parseInt(props.selectedYYYYMM.substring(4, 6)),
-        0,
-    ).getDay();
+        parseInt(props.selectedYYYYMM.substring(4, 6)) - 1,
+        1,
+    ).getDay(); // Sunday - Saturday : 0 - 6
     const lastDay = new Date(
         parseInt(props.selectedYYYYMM.substring(0, 4)),
         parseInt(props.selectedYYYYMM.substring(4, 6)),
@@ -19,13 +19,20 @@ const CalendarBody = (props: Props) => {
     ).getDate();
     const tableData: JSX.Element[] = [];
     const setTableData = () => {
-        const rowData = [];
+        let rowData = [];
         let weekdayIdx = weekdayIdxOfFirstDay;
+        let emptySlotCount = 0;
+        while (emptySlotCount < weekdayIdx) {
+            rowData.push(<td key={`Slot_${emptySlotCount}`}></td>);
+            emptySlotCount++;
+        }
         for (let i = firstDay; i <= lastDay; i++) {
             rowData.push(<td key={i}>{i}</td>);
             weekdayIdx++;
-            if (weekdayIdx === 7) {
+            if (weekdayIdx === 7 || i === lastDay) {
                 tableData.push(<tr key={i}>{rowData}</tr>);
+                rowData = [];
+                weekdayIdx = 0;
             }
         }
     };
