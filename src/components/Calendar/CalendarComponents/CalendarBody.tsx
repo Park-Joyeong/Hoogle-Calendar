3ffa3323b8ae9af0152fcefef09bfa6e0b541ed9
Park Joyeong <drhamst@gmail.com>
2022-04-12 17:51:57 +0900

    Calendar-body-table css

--- src/components/Calendar/CalendarComponents/CalendarBody.tsx
+++ src/components/Calendar/CalendarComponents/CalendarBody.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { MONTHS } from "./months";
+import "../../../css/calendar-body.css";
 
 export interface Props {
     selectedYYYYMM: string;
@@ -23,16 +24,28 @@ const CalendarBody = (props: Props) => {
         let weekdayIdx = weekdayIdxOfFirstDay;
         let emptySlotCount = 0;
         while (emptySlotCount < weekdayIdx) {
-            rowData.push(<td key={`Slot_${emptySlotCount}`}></td>);
+            rowData.push(<td key={`Upper_Slot_${emptySlotCount}`} className="calendar-body-td"></td>);
             emptySlotCount++;
         }
         for (let i = firstDay; i <= lastDay; i++) {
-            rowData.push(<td key={i}>{i}</td>);
+            rowData.push(
+                <td key={i} className="calendar-body-td">
+                    {i}
+                </td>,
+            );
             weekdayIdx++;
-            if (weekdayIdx === 7 || i === lastDay) {
+            if (weekdayIdx === 7) {
                 tableData.push(<tr key={i}>{rowData}</tr>);
                 rowData = [];
                 weekdayIdx = 0;
+            } else if (i === lastDay) {
+                emptySlotCount = 0;
+                while (weekdayIdx < 7) {
+                    rowData.push(<td key={`Lower_Slot_${emptySlotCount}`} className="calendar-body-td"></td>);
+                    weekdayIdx++;
+                    emptySlotCount++;
+                }
+                tableData.push(<tr key={i}>{rowData}</tr>);
             }
         }
     };
@@ -43,7 +56,7 @@ const CalendarBody = (props: Props) => {
                 {(MONTHS as any)[props.selectedYYYYMM.substring(4, 6)]}&nbsp;
                 {props.selectedYYYYMM.substring(0, 4)}
             </h1>
-            <table>
+            <table className="calendar-body-table">
                 <tbody>{tableData}</tbody>
             </table>
         </div>
